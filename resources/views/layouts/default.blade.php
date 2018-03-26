05e4b81479f08737703d426036586b31a673479d
zhangming1 <moresuccess2@gmail.com>
2018-03-26 13:19:32 +0000

    update-css5

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,18 +5,18 @@
     <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-    <header class="navbar navbar-fixed-top navbar-inverse">
-        <div class="container">
-            <div class="col-md offset-1 col-md 10">
-             <a href="/" id="logo">Sample app</a>
-                <nav>
-                 <ul class="nav navbar-nav navbar-right">
-                     <li><a href="/help">帮助</a></li>
-                     <li><a href="#">登录</a></li>
-                  </ul>
-                 </nav>
-             </div>
+<header class="navbar navbar-fixed-top navbar-inverse">
+      <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+          <a href="/" id="logo">Sample App</a>
+          <nav>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="/help">帮助</a></li>
+              <li><a href="#">登录</a></li>
+            </ul>
+          </nav>
         </div>
+      </div>
     </header>
     <div class="container">
       @yield('content')
