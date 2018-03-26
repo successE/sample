1c545e3f223b42706d950f0b147a27ed15aa480e
zhangming1 <moresuccess2@gmail.com>
2018-03-26 08:37:51 +0000

    update1

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,4 @@
 
 Route::get('/','StaticPagesController@home');
 Route::get('/help','StaticPagesController@help');
-Route::get('/about','StaticPagesController/about');
+Route::get('/about','StaticPagesController@about');
