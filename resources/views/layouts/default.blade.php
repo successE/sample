959bbc7b589bc0124f40488676ddcb383c108302
zhangming1 <moresuccess2@gmail.com>
2018-03-26 13:14:41 +0000

    update-css4

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -8,13 +8,14 @@
     <header class="navbar navbar-fixed-top navbar-inverse">
         <div class="container">
             <div class="col-md offset-1 col-md 10">
-            <a href="/" id="logo">Sample app</a></div>
-             <nav>
-               <ul class="nav navbar-nav navbar-right">
-                <li><a href="/help">帮助</a></li>
-                <li><a href="#">登录</a></li>
-               </ul>
-             </nav>
+             <a href="/" id="logo">Sample app</a>
+                <nav>
+                 <ul class="nav navbar-nav navbar-right">
+                     <li><a href="/help">帮助</a></li>
+                     <li><a href="#">登录</a></li>
+                  </ul>
+                 </nav>
+             </div>
         </div>
     </header>
     <div class="container">
