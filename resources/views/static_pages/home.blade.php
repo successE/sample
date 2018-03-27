945ae35271ed5fb04829779533d8f98f0f9c6233
zhangming1 <moresuccess2@gmail.com>
2018-03-27 04:07:26 +0000

    Finish layout and routes

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -7,7 +7,11 @@
         的实例项目主页
     </p>
     <p>一切，将从这里开始</p>
-    <p class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</p>
+    <p>
+        <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+
+        </p>
+
     </div>
 
 
