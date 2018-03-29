5701bfab37c7427f222c3bb4cde656d25127ca60
zhangming1 <moresuccess2@gmail.com>
2018-03-29 08:25:29 +0000

    Finish login and logout

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Route::get('signup',"UsersController@create")->name('signup');
 
 
 Route::resource('users','UsersController');
+
+Route::get('login','SessionController@create')->name('login');
+Route::post('login','SessionController@store')->name('login');
+Route::delete('logout','SessionController@destory')->name('logout');
